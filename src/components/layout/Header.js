88f3668379bc4df1753ecdebaf8d8751dd82d2c3
Umit Turk <uyt.turk@gmail.com>
2021-06-17 00:16:27 +0300

    styling app

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -7,12 +7,12 @@ export const Header = () => {
         <header className="header" data-testid="header">
             <nav>
                 <div className="logo">
-                    <img src="/images/logo/png" alt="Todoist" />
+                    <img src="/images/logo.png" alt="Todoist" />
                 </div>
                 <div className="settings">
                     <ul>
-                        <li>+</li>
-                        <li>
+                        <li data-testid="quick-add-task-action" className="settings__add">+</li>
+                        <li data-testid="dark-mode-action" className="settings__darkmode">
                             <FaPizzaSlice />
                         </li>
                     </ul>
--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -8,16 +8,41 @@ import {
 export const Sidebar = () => (
   <div className="sidebar" data-test-id="sidebar">
     <ul className="sidebar__generic">
-      <li><span><FaInbox /></span><span>Inbox</span></li>
-      <li><span><FaRegCalendar /></span><span>Today</span></li>
-      <li><span><FaRegCalendarAlt /></span><span>Next 7 days</span></li>
+      <li
+      data-testid="inbox"
+      data-testid="inbox"
+      className="inbox"
+      >
+        <span>
+          <FaInbox />
+        </span>
+        <span>Inbox</span>
+      </li>
+      <li
+      data-testid="today"
+      className="today"
+      >
+        <span>
+          <FaRegCalendar />
+        </span>
+        <span>Today</span>
+      </li>
+      <li
+      className="next_7"
+      data-testid="next_7"
+      >
+        <span>
+          <FaRegCalendarAlt />
+        </span>
+        <span>Next 7 days</span>
+      </li>
     </ul>
-
     <div className="sidebar__middle">
-        <span><FaChevronDown /></span>
-        <h2>Projects</h2>
+      <span>
+        <FaChevronDown />
+      </span>
+      <h2>Projects</h2>
     </div>
-
     <ul className="sidebar__projects">Projects will be here!</ul>
     Add project Component here!!
   </div>
